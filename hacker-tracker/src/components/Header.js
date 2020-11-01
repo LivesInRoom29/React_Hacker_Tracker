e50730e3ebc696d47f29a606ed890f01e5ce6044
LIR29 <61219066+LivesInRoom29@users.noreply.github.com>
2020-11-01 18:01:33 -0500

    Change class name to style heading

--- hacker-tracker/src/components/Header.js
+++ hacker-tracker/src/components/Header.js
@@ -4,7 +4,7 @@ function Header() {
   return (
     <header className="jumbotron jumbotron-fluid text-center">
       <h1 className="display-3">Hacker Tracker</h1>
-      <h2 className="display-4">Employee Directory</h2>
+      <h2 className="subtitle">Employee Directory</h2>
       <p className="lead">
         Use the search bar to filter results or sort by using the arrows in the heading.
       </p>
