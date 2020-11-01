589eb94bc8bdbf76e96221a715b5f56606c1e490
LIR29 <61219066+LivesInRoom29@users.noreply.github.com>
2020-11-01 12:14:59 -0500

    add comment to API component

--- hacker-tracker/src/utils/API.js
+++ hacker-tracker/src/utils/API.js
@@ -1,6 +1,7 @@
 import axios from "axios";
 const URL = "https://randomuser.me/api/?results=200&nat=us";
 
+// This API does not require a key nor a query string
 export default {
   search: () => axios.get(URL)
 };
