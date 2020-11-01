67c903d68375dd2ee4a96a2192423dfc21950fc3
LIR29 <61219066+LivesInRoom29@users.noreply.github.com>
2020-11-01 17:12:20 -0500

    cahnge class to className on font awesome icons

--- hacker-tracker/src/components/EmployeeTable.js
+++ hacker-tracker/src/components/EmployeeTable.js
@@ -3,7 +3,6 @@ import SortArray from "../utils/SortArray";
 
 function EmployeeTable(props) {
   const { employees } = props;
-  console.log("employees: ", employees);
   // To add sorting ability; sortConfig is an object that
   // contains a key for what to sort by and a direction for sorting
   const [ sortConfig, setSortConfig ] = React.useState({
@@ -22,6 +21,7 @@ function EmployeeTable(props) {
 
   const sortedEmployees = SortArray(employees, sortConfig.key, sortConfig.direction);
 
+  
   return (
     <table className="table table-striped">
       <caption>Our Employees</caption>
@@ -30,18 +30,18 @@ function EmployeeTable(props) {
           <th scope="col">Picture</th>
           <th scope="col">
             <button type="button" onClick={() => requestSort("name")}>
-              Name  <i class="fas fa-sort"></i>
+              Name  <i className="fas fa-sort"></i>
             </button>
           </th>
           <th scope="col">Phone</th>
           <th scope="col">
             <button type="button" onClick={() => requestSort("email")}>
-              Email  <i class="fas fa-sort"></i>
+              Email  <i className="fas fa-sort"></i>
             </button>
           </th>
           <th scope="col">
             <button type="button" onClick={() => requestSort("dob")}>
-              DOB  <i class="fas fa-sort"></i>
+              DOB  <i className="fas fa-sort"></i>
             </button>
           </th>
         </tr>
